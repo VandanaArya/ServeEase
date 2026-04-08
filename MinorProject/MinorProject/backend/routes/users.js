426778d643f29f4d1af00d9239f4e7a70a892cfc
Vandana Arya <vandanaarya987@gmail.com>
2026-04-08 08:39:27 +0530

    Clean up repository by removing node_modules

--- MinorProject/MinorProject/backend/routes/users.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-
-// Required fixing for auth.js in server.js, just making sure the logic flows
-// Also need to add bookings to server.js
